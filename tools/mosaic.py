130bd145e89bdc284890cb8c9394bdf03f096c5b
rhinejoel <joelrhine7@gmail.com>
2024-11-11 12:14:41 -0500

    Remove rasterio, add option for vrt or tif file generation

--- tools/mosaic.py
+++ tools/mosaic.py
@@ -3,78 +3,35 @@ from osgeo import gdal
 from typing import Literal
 from rasterio.merge import merge
 
-
 class CreateMosaic():
     def __init__(self):
         gdal.UseExceptions()
-        
 
-    def merge_single(self, dir:str, root:str, mprocess:Literal[1, 2]) -> None:
-        """
-        dir: single folder containing .tif files to be merged
-        root: parent folder path of dir
-        """
-        merged_folder = os.path.join(root, 'merged')
-        if not os.path.exists(merged_folder):
-            print("merged FOLDER DOES NOT EXIST. CREATING.")
-            os.makedirs(merged_folder)
-
-        loc = os.path.join(root, dir)
-        out_file = "Merged_{}.tif".format(dir)
-        out = os.path.join(merged_folder, out_file)
-        extension = "*.tif"
-        q = os.path.join(loc, extension)
-        files = glob.glob(q)
-        print("Found {} Files. Merging in {} Folder".format(len(files), dir))
-
-        if mprocess == 1:
-            self.use_rasterio(files=files, out=out)
-        else:
-            self.use_gdal(files=files, out=out, dir=dir)
-
-    def merge_multiple(self, dirs:list[str], root:str, mprocess:Literal[1, 2]) -> None:
+    def create_mosaic(self, dirs:list[str], root:str, output:Literal[1, 2] = 1) -> None:
         """
         dirs: list of folders containing .tif files to be merged
         root: parent folder path of dirs
+        output: default 1 - Only generates a .vrt file (faster). 2 - Generates a .tif file along with a .vrt file (slower and larger disk space)
         """
-        merged_folder = os.path.join(root, 'merged')
+        period = os.path.basename(root)
+        merged_folder = os.path.join(root, '{}_merged'.format(period))
         if not os.path.exists(merged_folder):
-            print("merged FOLDER DOES NOT EXIST. CREATING.")
+            print("'merged' folder does not exist. Creating...")
             os.makedirs(merged_folder)
 
         for dir in dirs:
             loc = os.path.join(root, dir)
-            out_file = "Merged_{}.tif".format(dir)
-            out = os.path.join(merged_folder, out_file)
             extension = "*.tif"
             q = os.path.join(loc, extension)
             files = glob.glob(q)
-            print("Found {} Files. Merging in {} Folder".format(len(files), dir))
-
-            if mprocess == 1:
-                self.use_rasterio(files=files, out=out)
+            if len(files) == 0:
+                print("{} is empty. Continuing to next directory".format(dir))
             else:
-                self.use_gdal(files=files, out=out, dir=dir)
+                print("Found {} Files. Merging in {} folder".format(len(files), dir))
 
-    def use_rasterio(self, files:list[str], out:str):
-        r =[]
-        for f in files:
-            s = rasterio.open(f)
-            r.append(s)
-        if len(r)>0:
-            mosaic, out_trans = merge(r)
-            out_meta = s.meta.copy()
-            out_meta.update({"driver": "GTiff",
-                        "height": mosaic.shape[1],
-                        "width": mosaic.shape[2],
-                        "transform": out_trans
-                        })
-            with rasterio.open(out, "w", **out_meta) as dest:
-                dest.write(mosaic)
-
-    def use_gdal(self, files:list[str], out:str, dir):
-        loc = os.path.dirname(out)
-        vrt_file = os.path.join(loc, 'Merged_{}.vrt'.format(dir))
-        vrt = gdal.BuildVRT(vrt_file, files)
-        # gdal.Translate(out, vrt, format="GTiff")
-        vrt = None
+                if output == 1:
+                    vrt_file = os.path.join(merged_folder, 'Merged_{}_{}.vrt'.format(period, dir))
+                    vrt = gdal.BuildVRT(vrt_file, files)
+                    if output == 2:
+                        gdal.Translate(merged_folder, vrt, format="GTiff")
+                    vrt = None
