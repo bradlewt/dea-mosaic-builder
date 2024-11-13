05a4df13de302cb1136e94f37cc20352497b1c72
rhinejoel <joelrhine7@gmail.com>
2024-11-13 11:41:05 -0500

    generate tif in create_mosiac

--- tools/mosaic.py
+++ tools/mosaic.py
@@ -6,31 +6,57 @@ class CreateMosaic():
     def __init__(self):
         gdal.UseExceptions()
 
-    def create_mosaic(self, dirs:list[str], root:str, output:Literal[1, 2] = 1) -> None:
+    def create_mosaic(self, indirs:list[str], root:str, a3:str, tifdir:str = None) -> None:
         """
-        dirs: list of folders containing .tif files to be merged
-        root: parent folder path of dirs
-        output: default 1 - Only generates a .vrt file (faster). 2 - Generates a .tif file along with a .vrt file (slower and larger disk space)
+        indirs: list of folders containing .tif files to be merged
+        root: parent folder path of indirs
+        a3: alpha3 code of the country
+        tifdir: output directory for .the tif files
         """
         period = os.path.basename(root)
         merged_folder = os.path.join(root, '{}_merged'.format(period))
+
         if not os.path.exists(merged_folder):
             print("'merged' folder does not exist. Creating...")
             os.makedirs(merged_folder)
 
-        for dir in dirs:
+        for dir in indirs:
             loc = os.path.join(root, dir)
-            extension = "*.tif"
-            q = os.path.join(loc, extension)
+            ext = "*.tif"
+            q = os.path.join(loc, ext)
             files = glob.glob(q)
             if len(files) == 0:
                 print("{} is empty. Continuing to next directory".format(dir))
             else:
                 print("Found {} Files. Merging in {} folder".format(len(files), dir))
+                outname = '{}_{}_merged_{}'.format(a3, period, dir)
+                vrt_file = os.path.join(merged_folder, outname + ".vrt")
 
-                if output == 1:
-                    vrt_file = os.path.join(merged_folder, 'Merged_{}_{}.vrt'.format(period, dir))
+                if os.path.exists(vrt_file):
+                    print("{}.vrt exists.".format(outname))
+                else:
+                    # create vrt if it does not exist
+                    print("Creating {}.vrt".format(outname))
                     vrt = gdal.BuildVRT(vrt_file, files)
-                    if output == 2:
-                        gdal.Translate(merged_folder, vrt, format="GTiff")
-                    vrt = None
+
+                if tifdir != None:
+                    # create tif if output dir is specified
+                    vrt = gdal.Open(vrt_file)
+                    tif_out = os.path.join(tifdir, outname + ".tif")
+                    print("Creating {}.tif".format(outname))
+                    gdal.Translate(tif_out, vrt, format="GTiff")
+
+    # def vrt2tif(self, indir:str, outdir:str) -> None:
+    #     ext = '*.vrt'
+    #     q = os.path.join(indir, ext)
+    #     files = glob.glob(q)
+    #     if len(files) == 0:
+    #         print("No .vrt files found. Check folder name or first generate .vrt files by running cm.create_mosiac()")
+    #     else:
+    #         print("Generating...")
+    #         for f in files:
+    #             outname = os.path.splitext(os.path.basename(f))[0]
+    #             print("Converting {}.vrt to {}.tif".format(outname, outname))
+    #             outpath = os.path.join(outdir, outname + ".tif")
+    #             vrt = gdal.Open(f)
+    #             gdal.Translate(outpath, vrt, format="GTiff")
