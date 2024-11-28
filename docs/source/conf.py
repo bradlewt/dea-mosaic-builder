f01eb22be2f15592061160f4bcaf5111f4cc9dd9
rhinejoel <joelrhine7@gmail.com>
2024-11-27 22:47:07 -0500

    Force guthuburl

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,4 +5,6 @@ author = "Joel Rhine"
 html_theme = "sphinx_rtd_theme"
 html_logo = "_static/rwd-header.png"
 
+github_url = "https://github.com/rhinejoel/dea-mosaic-builder/tree/main/docs/source"
+
 html_theme_options = {"logo_only": True}
