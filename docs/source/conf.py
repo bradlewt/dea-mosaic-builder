9b29399bb28442cb66ff665bd07c5a59b0027924
rhinejoel <joelrhine7@gmail.com>
2024-11-27 22:31:45 -0500

    Edit docs logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,4 +3,6 @@ project_copyright = "%Y, Joel Rhine"
 author = "Joel Rhine"
 
 html_theme = "sphinx_rtd_theme"
-html_logo = "_static/rwd-header_.png"
+html_logo = "_static/rwd-header.png"
+
+html_theme_options = {"logo_only": True}
