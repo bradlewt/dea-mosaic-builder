67ec7e52c58b56d97f4f20c43727f0d8e5263f09
rhinejoel <joelrhine7@gmail.com>
2024-11-28 09:45:21 -0500

    Add light and dark logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,13 @@ project_copyright = "%Y, Joel Rhine"
 author = "Joel Rhine"
 
 html_theme = "furo"
-html_logo = "_static/rwd-header.png"
+# html_logo = "_static/rwd-header.png"
 
 github_url = "https://github.com/rhinejoel/dea-mosaic-builder/tree/main/docs/source"
 
 # html_theme_options = {"logo_only": True}
-html_theme_options = {"sidebar_hide_name": True}
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "rwd-header-light.png",
+    "dark_logo": "rwd-header-dark.png",
+}
