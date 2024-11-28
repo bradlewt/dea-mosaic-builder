4fdb4e2e47a7bea6333b04f2c84dbd1382c2a450
rhinejoel <joelrhine7@gmail.com>
2024-11-27 22:01:59 -0500

    Fix docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,5 +2,5 @@ project = "DEA Mosaic Builder"
 project_copyright = "%Y, Joel Rhine"
 author = "Joel Rhine"
 
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 html_logo = "_static/rwd-header.png"
