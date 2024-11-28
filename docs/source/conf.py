763584b3cdacefece89a3e7f5d677e075bed92bd
rhinejoel <joelrhine7@gmail.com>
2024-11-28 09:54:31 -0500

    Add _static path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,7 @@ html_theme = "furo"
 github_url = "https://github.com/rhinejoel/dea-mosaic-builder/tree/main/docs/source"
 
 # html_theme_options = {"logo_only": True}
+html_static_path = ["_static"]
 html_theme_options = {
     "sidebar_hide_name": True,
     "light_logo": "rwd-header-light.png",
